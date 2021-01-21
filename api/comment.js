21d1855798cf7f7afc80857809b0bcf1e359669b
machard <matthieu__@hotmail.fr>
2021-01-21 15:39:54 +0100

    add a comment endpoint

---
+++ api/comment.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { createAppAuth } from "@octokit/auth-app";
+import atob from "atob";
+
+module.exports = async (req, res) => {
+  const auth = createAppAuth({
+    appId: process.env.GHBOT_APPID,
+    privateKey: atob(process.env.GHBOT_PRIVATEKEY),
+    installationId: process.env.GHBOT_INSTALLATIONID,
+    clientId: process.env.GHBOT_CLIENTID,
+    clientSecret: process.env.GHBOT_SECRET,
+  });
+  const { token } = await auth({
+    type: "installation",
+    repositories: ["LedgerHQ/ledger-live-desktop"]
+  });
+  const { body } = req;
+
+  const baseUrl = `https://api.github.com/repos/${req.query.owner}/${req.query.repo}`;
+
+  try {
+    const result = await axios
+      .post(
+        `${baseUrl}/issues/${req.query.issueId}/comments`,
+        JSON.stringify({
+          body: req?.body?.comment
+        }),
+        {
+          headers: {
+            'Accept': 'application/vnd.github.v3+json',
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+    res.status(200).json(result.data);
+  } catch(e) {
+    res.status(500).json(e);
+  }
+}
